b70bbfc0f02741e6a4ef4fe6d0c60425fb6de0cc
rizkyrajitha <rajithagunathilake@gmail.com>
2021-07-15 14:30:05 +0530

    added bastion ec2 and modifications

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,17 +1,19 @@
 resource "aws_vpc" "vpcawseducateiacdemo" {
- cidr_block       = "10.0.0.0/16"
- tags = {
-   "Name" = "vpcawseducateiacdemo"
- }
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    "Name" = "vpcawseducateiacdemo"
+  }
 }
 
 resource "aws_subnet" "publicsubnetawseducateiacdemo" {
-  vpc_id = aws_vpc.vpcawseducateiacdemo.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id                  = aws_vpc.vpcawseducateiacdemo.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+
 }
 
 resource "aws_subnet" "privatesubnetawseducateiacdemo" {
-  vpc_id = aws_vpc.vpcawseducateiacdemo.id
+  vpc_id     = aws_vpc.vpcawseducateiacdemo.id
   cidr_block = "10.0.2.0/24"
 }
 
@@ -24,26 +26,65 @@ resource "aws_internet_gateway" "igwawseducatedemoiac" {
 
 resource "aws_route_table" "crtawseducatedemoiac" {
   vpc_id = aws_vpc.vpcawseducateiacdemo.id
+  # route {
+  #   cidr_block = "0.0.0.0/0"
+  #   gateway_id = aws_internet_gateway.igwawseducatedemoiac.id
+  # }
+
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igwawseducatedemoiac.id
+  }
 
-  } 
 
-tags = {
-  "Name" = "croutetableawseducatedemoiac"
+  tags = {
+    "Name" = "croutetableawseducatedemoiac"
+  }
 }
+
+resource "aws_route_table_association" "racawseducatedemoiac" {
+  subnet_id      = aws_subnet.publicsubnetawseducateiacdemo.id
+  route_table_id = aws_route_table.crtawseducatedemoiac.id
 }
+
+resource "aws_security_group" "awseducateicademosg" {
+  name = "awseducateicademosg"
+
+  vpc_id = aws_vpc.vpcawseducateiacdemo.id
+
+  ingress {
+    description      = "http"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+}
+
+# resource "aws_key_pair" "privatekeyviabastion" {
+#   key_name = "privatekeyec2"
+
+# }
 
 
 resource "aws_instance" "ec2privateawseducateiacdemo" {
 
   # availability_zone = var.az
-  ami               = "ami-09e67e426f25ce0d7" # ubuntu 20 image
-  instance_type     = "t2.micro"
-  tags              = { Name = var.private_instance_name }
-  key_name          = "awseducateiacdemo"
-  # security_groups = [ aws_security_group.awseducateicademosg.name]
+  ami           = "ami-09e67e426f25ce0d7" # ubuntu 20 image
+  instance_type = "t2.micro"
+  tags          = { Name = var.private_instance_name }
+  key_name      = "awseducateiacdemo"
+  vpc_security_group_ids = [aws_security_group.awseducateicademosg.id]
   subnet_id = aws_subnet.privatesubnetawseducateiacdemo.id
 
   root_block_device {
@@ -60,12 +101,12 @@ resource "aws_instance" "ec2privateawseducateiacdemo" {
 resource "aws_instance" "ec2publicawseducateiacdemo" {
 
   # availability_zone = var.az
-  ami               = "ami-09e67e426f25ce0d7" # ubuntu 20 image
-  instance_type     = "t2.micro"
-  tags              = { Name = var.public_instance_name }
-  key_name          = "awseducateiacdemo"
-  # security_groups = [ aws_security_group.awseducateicademosg.name]
-  subnet_id = aws_subnet.publicsubnetawseducateiacdemo.id
+  ami                    = "ami-09e67e426f25ce0d7" # ubuntu 20 image
+  instance_type          = "t2.micro"
+  tags                   = { Name = var.public_instance_name }
+  key_name               = "awseducateiacdemo"
+  vpc_security_group_ids = [aws_security_group.awseducateicademosg.id]
+  subnet_id              = aws_subnet.publicsubnetawseducateiacdemo.id
 
   root_block_device {
     volume_size           = 10
